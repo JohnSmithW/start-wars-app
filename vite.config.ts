5e7072b1b3ca25d8afb9fbf1abbdf765c675469e
JohnSmithW <salapin.danil1@mail.ru>
2025-02-02 21:26:17 +0200

    Refactored code and added build for online publishing

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
