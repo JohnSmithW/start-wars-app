f87fc21c03742a3235568219f2bea164229b0795
JohnSmithW <salapin.danil1@mail.ru>
2025-02-02 22:15:44 +0200

    added base path

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
