0ba6b4446821478f6be370f7e696aaecf4975c2a
JohnSmithW <salapin.danil1@mail.ru>
2025-02-09 15:41:51 +0200

    Added deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
